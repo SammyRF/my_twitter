2158e6d4bcb53c96d2df797c87554059f2602692
SammyRF <rong.fan@live.de>
2021-08-30 07:31:58 +0200

    add comments model

--- utils/test_helpers.py
+++ utils/test_helpers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from tweets.models import Tweet
 from friendships.models import Friendship
+from comments.models import Comment
 
 
 class TestHelpers:
@@ -14,9 +15,13 @@ class TestHelpers:
         )
 
     @classmethod
-    def create_tweet(cls, user, content='default content'):
+    def create_tweet(cls, user, content='default tweet content'):
         return Tweet.objects.create(user=user, content=content)
 
     @classmethod
     def create_friendship(cls, from_user, to_user):
         return Friendship.objects.create(from_user=from_user, to_user=to_user)
+
+    @classmethod
+    def create_comment(cls, user, tweet, content="default comment content"):
+        return Comment.objects.create(user=user, tweet=tweet, content=content)
