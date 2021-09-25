e2b51d64e1b29e57e6d22b05f2526c6c7a154dcd
SammyRF <rong.fan@live.de>
2021-09-25 22:13:27 +0200

    remove unused code

--- accounts/services.py
+++ accounts/services.py
@@ -7,28 +7,6 @@ from utils.cache_helpers import USER_PATTERN, USER_PROFILE_PATTERN, project_cach
 class UserService:
 
     @classmethod
-    def get_user_through_cache(cls, user_id):
-        key = USER_PATTERN.format(user_id=user_id)
-
-        # get user from cache first
-        user = project_cache.get(key)
-        if user:
-            return user
-
-        # if not found in cache, go to sql
-        try:
-            user = User.objects.get(id=user_id)
-            project_cache.set(key, user)
-        except User.DoesNotExist:
-            user = None
-        return user
-
-    @classmethod
-    def invalidate_user(cls, user_id):
-        key = USER_PATTERN.format(user_id=user_id)
-        project_cache.delete(key)
-
-    @classmethod
     def get_user_profile_through_cache(cls, user_id):
         key = USER_PROFILE_PATTERN.format(user_id=user_id)
 
